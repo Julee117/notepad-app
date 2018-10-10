10b0babbdce9461401ea339025d94fa9f3ed9c56
github username <juliannne@gmail.com>
2018-10-10 16:36:56 -0400

    Add create, edit, update, destroy methods

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,4 +1,5 @@
 class NotesController < ApplicationController
+  before_action :find_note, only: [:show, :edit, :update, :destroy]
   def index
     @notes = Note.where(user_id: current_user)
   end
@@ -9,7 +10,32 @@ class NotesController < ApplicationController
   def new
     @note = current_user.notes.build
   end
-  
+
+  def create
+    @note = current_user.notes.build(note_params)
+    if @note.save
+      redirect_to note_path(@note)
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @note.update(note_params)
+      redirect_to note_path(@note)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @note.destroy
+    redirect_to notes_path
+  end
+
   private
 
   def find_note
