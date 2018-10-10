09526ff69cb1b26cb1d28539eb9712c6d1745911
github username <juliannne@gmail.com>
2018-10-10 16:08:52 -0400

    Add private methods

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,2 +1,11 @@
 class NotesController < ApplicationController
+  private
+
+  def find_note
+    @note = Note.find(params[:id])
+  end
+
+  def note_params
+    params.require(:note).permit(:title, :content, :user_id)
+  end
 end
